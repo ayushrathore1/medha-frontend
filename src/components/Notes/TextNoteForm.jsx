4ba9653fb0c3a4be60190dd124241aa85ca05e96
Ayush Rathore <131510947+ayushrathore1@users.noreply.github.com>
2025-10-30 22:07:53 +0530

    Update TextNoteForm.jsx

--- src/components/Notes/TextNoteForm.jsx
+++ src/components/Notes/TextNoteForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-const API_BASE = "https://medha-backend.onrender.com/";
+const API_BASE = "https://medha-backend.onrender.com";
 
 const TextNoteForm = ({ subjectId, onNoteCreated, token }) => {
   const [title, setTitle] = useState("");
